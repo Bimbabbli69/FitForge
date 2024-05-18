c4c27fa5162c80ca764f817a3811c6bcd5db69ce
Bimbabbli69 <elliot.brandin@hotmail.com>
2024-05-18 19:11:34 +0200

    Finished exercise Routs

--- Controllers/exercises.js
+++ Controllers/exercises.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const Exercise = require('../models/exercise')
+const exercise = require('../models/exercise')
+const musclegroup = require('../models/musclegroup')
+const Musclegroup = require('../models/musclegroup')
 
 // All exercises Controller
 router.get('/', async (req, res) => {
@@ -28,17 +31,78 @@ router.get('/new', (req, res) => {
 router.post('/', async (req, res) => {
     const exercise = new Exercise({
         name: req.body.name
-    });
+    })
     try {
         const newExercise = await exercise.save()
-        // res.redirect(`exercises/${newExercise.id}`)
-        res.redirect(`exercises`)
+        res.redirect(`exercises/${newExercise.id}`)
     } catch {
         res.render('exercises/new', {
             exercise: exercise, 
             errorMessage: 'Error creating Exercise'
-        });
+        })
     }
 })
+
+router.get('/:id', async (req, res) => {
+    try {
+        const exercise = await Exercise.findById(req.params.id)
+        const musclegroups = await Musclegroup.find({ exercise: exercise.id}).limit(6).exec()
+        res.render('exercises/show', {
+            exercise: exercise,
+            musclegroupsByExercise: musclegroups
+        })
+    } catch {
+        res.redirect('/')
+    }
+})
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const exercise = await Exercise.findById(req.params.id)
+        if (exercise == null) {
+            return res.redirect('/exercises');
+        }
+        res.render('exercises/edit', { exercise: exercise })
+    } catch {
+        res.redirect('/exercises')
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    let exercise
+    try {
+        exercise = await Exercise.findById(req.params.id)
+        exercise.name = req.body.name
+        await exercise.save()
+        res.redirect(`/exercises/${exercise.id}`)
+    } catch {
+        if (exercise == null) {
+            res.redirect('/')
+        } else {
+            res.render('exercises/edit', {
+                exercise: exercise, 
+                errorMessage: 'Error updating Exercise'
+            })
+        }
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    let exercise;
+    try {
+      exercise = await Exercise.findById(req.params.id);
+      if (!exercise) {
+        return res.redirect('/');
+      }
+      await exercise.deleteOne();
+      res.redirect('/exercises');
+    } catch (err) {
+      if (err.message === 'This exercise has muscle groups still') {
+        res.redirect(`/exercises/${exercise._id}`);
+      } else {
+        res.redirect('/');
+      }
+    }
+  });
 
 module.exports = router
--- models/exercise.js
+++ models/exercise.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Musclegroup = require('./musclegroup')
 
 const exerciseSchema = new mongoose.Schema({
     name: {
@@ -6,5 +7,19 @@ const exerciseSchema = new mongoose.Schema({
         required: true
     }
 })
+
+// Stoppar från att radera en exercise med en musclegroup
+exerciseSchema.pre('deleteOne', { document: true, query: false }, async function(next) {
+    try {
+      const musclegroups = await Musclegroup.find({ exercise: this._id });
+      if (musclegroups.length > 0) {
+        next(new Error('This exercise has muscle groups still'));
+      } else {
+        next();
+      }
+    } catch (err) {
+      next(err);
+    }
+  });
 
 module.exports = mongoose.model('Exercise', exerciseSchema)
